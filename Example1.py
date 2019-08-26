1b574affda5e3d736ba5fd45b00d3c457f3462b8
ssalian <sushma.salian@unico.com.au>
2019-08-26 14:22:41 +1000

    Updated metrics

--- Example1.py
+++ Example1.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
     log_param("param1", 5)
 
     # Log a metric; metrics can be updated throughout the run
-    log_metric("foo", 10)
-    log_metric("foo", 20)
-    log_metric("foo", 30)
+    log_metric("foo", 35)
+    log_metric("foo", 15)
+    log_metric("foo", 13)
 
     # Log an artifact (output file)
     with open("output.txt", "w") as f:
-        f.write("Hello world!!!")
+        f.write("Hello world again!!!")
     log_artifact("output.txt")
